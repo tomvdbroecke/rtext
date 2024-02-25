8536deea37b354929187c098285c83dbab7e7896
tomvdbroecke <tom@tvdb.me>
2024-02-25 15:54:02 +0100

    setup arguments with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// Uses
+use clap::Parser;
+
+// Defaults
+const DEFAULT_PATH: &str = "random.txt";
+const DEFAULT_WORDS_PER_LINE: u64 = 10;
+const DEFAULT_AMOUNT_OF_LINES: u64 = 10;
+
+// Struct for arguments
+#[derive(Parser)]
+struct Args {
+    #[arg(default_value_t = String::from(DEFAULT_PATH))]
+    path: String,
+    #[arg(default_value_t = DEFAULT_WORDS_PER_LINE)]
+    words_per_line: u64,
+    #[arg(default_value_t = DEFAULT_AMOUNT_OF_LINES)]
+    amount_of_lines: u64,
+}
+
+// Main function
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!")
 }
