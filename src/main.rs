dc51c515314cd5e59a40f7cf648eced981b12466
tomvdbroecke <tom@tvdb.me>
2024-02-25 16:41:20 +0100

    line, word and text file generation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+// Modules
+mod process_command;
+
 // Uses
-use clap::Parser;
+use clap::{error::ErrorKind, CommandFactory, Parser};
+use process_command::process_command;
+use anyhow::Result;
 
 // Defaults
 const DEFAULT_PATH: &str = "random.txt";
@@ -9,15 +14,29 @@ const DEFAULT_AMOUNT_OF_LINES: u64 = 10;
 // Struct for arguments
 #[derive(Parser)]
 struct Args {
-    #[arg(default_value_t = String::from(DEFAULT_PATH))]
+    #[arg(short, long, default_value_t = String::from(DEFAULT_PATH))]
     path: String,
-    #[arg(default_value_t = DEFAULT_WORDS_PER_LINE)]
+    #[arg(short, long, default_value_t = DEFAULT_WORDS_PER_LINE)]
     words_per_line: u64,
-    #[arg(default_value_t = DEFAULT_AMOUNT_OF_LINES)]
+    #[arg(short, long, default_value_t = DEFAULT_AMOUNT_OF_LINES)]
     amount_of_lines: u64,
 }
 
 // Main function
-fn main() {
-    println!("Hello, world!")
+fn main() -> Result<()> {
+    // Parse the arguments
+    let args: Args = Args::parse();
+
+    // Process the command, if an error occurs, format it the same way as 'clap'
+    if let Err(error) = process_command(args) {
+        let mut cmd = Args::command();
+        if let Some(source) = error.source() {
+            cmd.error(ErrorKind::Io, format!("{}\n\ncause: {}", &error, source)).exit()
+        } else {
+            cmd.error(ErrorKind::Io, format!("{}", &error)).exit()
+        }
+    }
+
+    // Return OK
+    Ok(())
 }
---
+++ src/process_command.rs
@@ -0,0 +1,52 @@
+// Uses
+use random_word::Lang;
+use crate::Args;
+use std::{fs::File, io::{BufWriter, Write}};
+
+
+// Process command function
+pub(crate) fn process_command(args: Args) -> Result<(), anyhow::Error> {
+    // Create file
+    let file = File::create(&args.path)?;
+
+    // Wrap file in a BufWriter
+    let mut buf_writer = BufWriter::new(file);
+
+    // Define variables to keep track of loop
+    let mut l: u64 = 0;
+    let mut w: u64;
+
+    // Loop over amount of lines
+    while l < args.amount_of_lines {
+        // Set word count (back) to 0
+        w = 0;
+
+        // Define line string
+        let mut line: String = Default::default();
+
+        // Loop over amount of words
+        while w < args.words_per_line {
+            // Generate random word and append it to the line
+            let word: &str = random_word::gen(Lang::En);
+            line.push_str(word);
+
+            // On each but the final word, add a space
+            if w + 1 < args.words_per_line {
+                line.push_str(" ");
+            }
+
+            // Go to next word
+            w = w + 1;
+        }
+
+        // Write line to file
+        line.push_str("\n");
+        buf_writer.write(line.as_bytes())?;
+
+        // Go to next line
+        l = l + 1;
+    }
+
+    // Return OK
+    Ok(())
+}
